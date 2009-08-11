5520e56e51c618d12f9d75ef2ae7175aca21b7fd
Alexander Ivanov <alexander.ivanov@redsolution.ru>
2009-08-11 23:15:35 +0600

    Fix templates and project_file

--- config/settings.py
+++ config/settings.py
@@ -12,10 +12,10 @@ admin_media_url = make_url(getattr(settings, 'ADMIN_MEDIA_PREFIX'))
 admin_media_path = make_path(os.path.join(os.path.dirname(admin.__file__), 'media'))
 
 project_name = os.environ['DJANGO_SETTINGS_MODULE'].split('.')[0]
-project_file = sys.argv[0]
+project_file = os.path.abspath(sys.argv[0])
 
-project_url = getattr(settings, 'PROJECT_URL', '127.0.0.1:8000')
-project_redirects = getattr(settings, 'PROJECT_REDIRECTS', [])
+project_url = getattr(settings, 'PROJECT_URL', 'www.example.com')
+project_redirects = getattr(settings, 'PROJECT_REDIRECTS', ['example.com'])
 project_auth = getattr(settings, 'PROJECT_AUTH', False)
 
 app_media_paths = get_app_media_paths(admin_media_url, admin_media_path)
