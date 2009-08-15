593842a9d8c234ada6460f0913b044ad06c2454c
Alexander Ivanov <alexander.ivanov@redsolution.ru>
2009-08-15 20:51:37 +0600

    Fix import in urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,9 @@
 from django.conf import settings
 from django.conf.urls.defaults import *
 
-from config.settings import media_url, config_media_paths
+from config.settings import media_url, media_paths
 
 urlpatterns = patterns('django.views.static', *[
     ('^%s%s(?P<path>.*)$' % (media_url[1:], url[1:]), 'serve', {
         'document_root': path} )
-    for url, path in config_media_paths ] )
+    for url, path in media_paths ] )
