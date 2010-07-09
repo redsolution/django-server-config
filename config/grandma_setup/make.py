643a31982d331a401f15956381383ddee36e42e0
Alexander Ivanov <alexander.ivanov@redsolution.ru>
2010-07-09 17:50:04 +0600

    Fix Make class for grandma_setup

--- config/grandma_setup/make.py
+++ config/grandma_setup/make.py
@@ -1,25 +1,22 @@
 import os
-from django.template.loader import render_to_string
-
 from grandma.make import BaseMake
 from grandma.models import GrandmaSettings
 from config.grandma_setup.models import ConfigSettings
 
 class Make(BaseMake):
     def make(self):
+        super(Make, self).make()
         config_settings = ConfigSettings.objects.get_settings()
-        data = render_to_string('config/grandma/settings.py', {
+        grandma_settings = GrandmaSettings.objects.get_settings()
+        grandma_settings.render_to('settings.py', 'config/grandma/settings.py', {
             'config_settings': config_settings,
         })
-        grandma_settings = GrandmaSettings.objects.get_settings()
-        grandma_settings.append_to('settings.py', data)
-        data = render_to_string('config/grandma/urls.py', {
+        grandma_settings.render_to('urls.py', 'config/grandma/urls.py', {
+            'config_settings': config_settings,
         })
-        grandma_settings = GrandmaSettings.objects.get_settings()
-        grandma_settings.append_to('urls.py', data)
-        data = render_to_string('grandma/buildout.cfg', {
+        grandma_settings.render_to(os.path.join('..', 'buildout.cfg'), 'config/grandma/buildout.cfg', {
+            'config_settings': config_settings,
         })
-        grandma_settings.append_to(os.path.join('..', 'buildout.cfg'), data)
-        data = render_to_string('grandma/develop.cfg', {
+        grandma_settings.render_to(os.path.join('..', 'develop.cfg'), 'config/grandma/develop.cfg', {
+            'config_settings': config_settings,
         })
-        grandma_settings.append_to(os.path.join('..', 'develop.cfg'), data)
--- config/grandma_setup/templates/config/grandma/settings.py
+++ config/grandma_setup/templates/config/grandma/settings.py
@@ -1,11 +1,11 @@
 # django-config
 INSTALLED_APPS += ['config']
 
-CONFIG_SITES = [{ % for site in config_settings.sites.all % }
+CONFIG_SITES = [{% for site in config_settings.sites.all %}
     '{{ site.site }}', { % endfor % }
 ]
-CONFIG_REDIRECTS = [{ % for site in config_settings.redirects.all % }
-    '{{ site.site }}', { % endfor % }
+CONFIG_REDIRECTS = [{% for site in config_settings.redirects.all %}
+    '{{ site.site }}', {% endfor %}
 ]
 #CONFIG_APP_MEDIA = {
 #    'pages': [
